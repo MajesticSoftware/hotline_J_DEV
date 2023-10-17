7dd336bfccafc0ed1e82097fd11b8c1b8618285a
macos <priynakgandhi000@gmail.com>
2023-10-17 18:00:27 +0530

    ui changes && mail to add

--- lib/view/main/app_starting_screen.dart
+++ lib/view/main/app_starting_screen.dart
@@ -1,13 +1,16 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:hotlines/generated/assets.dart';
 import 'package:hotlines/theme/app_color.dart';
 import 'package:hotlines/utils/extension.dart';
 import 'package:hotlines/view/widgets/game_widget.dart';
 
 import '../../constant/shred_preference.dart';
+import '../../theme/helper.dart';
 import '../sports/gameListing/game_listing_screen.dart';
 
 class AppStartScreen extends StatelessWidget {
@@ -47,10 +50,68 @@ class AppStartScreen extends StatelessWidget {
             "Getting Started:".appCommonText(
                 weight: FontWeight.bold, color: yellowColor, size: 25),
             20.H(),
-            dialogCard(
-                image: Assets.imagesBall,
-                title:
-                    'Pick a Sport - SEC football fan? Jump to NCAAF. Baltimore Ravens fan? Jump to NFL to see their next game.'),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Image.asset(
+                  Assets.imagesBall,
+                  height: MediaQuery.of(Get.context!).size.width * .055,
+                  fit: BoxFit.contain,
+                ),
+                10.W(),
+                Expanded(
+                    child: RichText(
+                  text: TextSpan(
+                      text: 'Pick a Sport - SEC football fan? Jump to ',
+                      style: GoogleFonts.nunitoSans(
+                        color: whiteColor,
+                        fontWeight: FontWeight.w400,
+                        fontSize:
+                            MediaQuery.of(Get.context!).size.height * .022,
+                      ),
+                      children: [
+                        TextSpan(
+                          text: 'NCAAF. ',
+                          style: GoogleFonts.nunitoSans(
+                            color: whiteColor,
+                            fontWeight: FontWeight.bold,
+                            fontSize:
+                                MediaQuery.of(Get.context!).size.height * .022,
+                          ),
+                        ),
+                        TextSpan(
+                          text: 'Baltimore Ravens fan? Jump to ',
+                          style: GoogleFonts.nunitoSans(
+                            color: whiteColor,
+                            fontWeight: FontWeight.w400,
+                            fontSize:
+                                MediaQuery.of(Get.context!).size.height * .022,
+                          ),
+                        ),
+                        TextSpan(
+                          text: 'NFL ',
+                          style: GoogleFonts.nunitoSans(
+                            color: whiteColor,
+                            fontWeight: FontWeight.bold,
+                            fontSize:
+                                MediaQuery.of(Get.context!).size.height * .022,
+                          ),
+                        ),
+                        TextSpan(
+                          text: 'NFL to see their next game.',
+                          style: GoogleFonts.nunitoSans(
+                            color: whiteColor,
+                            fontWeight: FontWeight.w400,
+                            fontSize:
+                                MediaQuery.of(Get.context!).size.height * .022,
+                          ),
+                        ),
+                      ]),
+                ))
+              ],
+            ),
+            10.H(),
             dialogCard(
                 image: Assets.imagesPopcorn,
                 title:
@@ -59,9 +120,44 @@ class AppStartScreen extends StatelessWidget {
                 image: Assets.imagesBook,
                 title:
                     'Learn - We provide you with head to head matchups and analysis to give you insights on trends.'),
-            dialogCard(
-                image: Assets.imagesHeart,
-                title: 'Share - Love HotlinesCB? Share with your friends!'),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Image.asset(
+                  Assets.imagesHeart,
+                  height: MediaQuery.of(Get.context!).size.width * .055,
+                  fit: BoxFit.contain,
+                ),
+                10.W(),
+                Expanded(
+                    child: RichText(
+                  text: TextSpan(
+                      text: 'Share - Love HotlinesCB? ',
+                      style: GoogleFonts.nunitoSans(
+                        color: whiteColor,
+                        fontWeight: FontWeight.w400,
+                        fontSize:
+                            MediaQuery.of(Get.context!).size.height * .022,
+                      ),
+                      children: [
+                        TextSpan(
+                          // recognizer: TapGestureRecognizer()
+                          //   ..onTap = () => launchInBrowser(Uri.parse(
+                          //       'https://testflight.apple.com/join/KOp4iOSH')),
+                          text: 'Share with your friends!',
+                          style: GoogleFonts.nunitoSans(
+                            color: whiteColor,
+                            fontWeight: FontWeight.bold,
+                            decoration: TextDecoration.underline,
+                            fontSize:
+                                MediaQuery.of(Get.context!).size.height * .022,
+                          ),
+                        ),
+                      ]),
+                ))
+              ],
+            ),
           ],
         ).paddingSymmetric(
             horizontal: MediaQuery.of(context).size.width * .05,
